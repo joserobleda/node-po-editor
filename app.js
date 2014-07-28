b651a3dd9eadb91248bc84eecac07510caff4ac6
Jose <joserobleda@gmail.com>
2014-07-28 09:15:34 +0200

    test right config

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-require('neasy').start();
+var fs 	= require('fs');
+var app = require('neasy');
+var cli = app.require('cli');
+
+
+if (app.config.locales == undefined || app.config.locales.path == undefined || fs.existsSync(app.config.locales.path) == false) {
+	return cli.error("Configure the locales:path in your neasy.json file!");
+}
+
+app.start();
