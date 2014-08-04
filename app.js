bee0086738ff5ed605e3f3d0813b67339c7df2b4
Jose <joserobleda@gmail.com>
2014-08-04 13:06:37 +0200

    parse ~ in routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
+var path = require('path');
 
 // arg as absolute route
 if (process.argv[2] !== undefined) {
-	process.argv[2] = require('path').resolve(process.argv[2]);
+	process.argv[2] = path.resolve(process.argv[2]);
 }
 
 // make sure we are in this folder to exec the app
@@ -12,9 +13,26 @@ process.chdir(__dirname);
 var fs 	= require('fs');
 var app = require('neasy');
 var cli = app.require('cli');
+var home = process.env.HOME;
 
-if (app.config.xgettext == undefined || app.config.xgettext.path == undefined || fs.existsSync(app.config.xgettext.path) == false) {
+
+
+if (app.config.xgettext == undefined || app.config.xgettext.path == undefined) {
 	return cli.error("Configure the xgettext:path in your config.json file!");
+}
+
+// replace ~ in case user wants relative path
+app.config.xgettext.path = app.config.xgettext.path.replace('~', home);
+if (fs.existsSync(app.config.xgettext.path) == false) {
+	return cli.error("The xgettext:path in your config.json file doesn't exists");
+}
+
+if (app.config.xgettext.sources === undefined) {
+	return cli.error("You should define a sources list");
+}
+
+for (var i in app.config.xgettext.sources) {
+	app.config.xgettext.sources[i] = app.config.xgettext.sources[i].replace('~', home);
 }
 
 app.start();
